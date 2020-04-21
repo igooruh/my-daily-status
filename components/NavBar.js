096bf3514c8ca54ee55de5a80c9bc7075f0fd37b
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-04-21 01:09:11 -0300

    Create navbar

---
+++ components/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Link from 'next/link'
+
+const NavLink = ({ href, children}) => {
+    return (
+        <Link href={href}>
+            <a className='p-2 hover:underline hover:text-red-800'>{children}</a>
+        </Link>
+    )
+}
+
+const NavBar = () => {
+    return (
+        <div>
+            <nav className='bg-gray-500 text-gray-700 text-center'>
+                <NavLink href='/sobre'>Sobre</NavLink>
+                <NavLink href='/cadastrar'>Cadastre-se</NavLink>
+                <NavLink href='/entrar'>Entrar</NavLink>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
