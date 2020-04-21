81bc776af2f14fcdda8b8babe3dd894351cf1208
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-04-21 01:07:57 -0300

    Create footer of the application

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className='py-4 text-center bg-gray-400 text-gray-600'>
+            My Daily Status é um projeto criado durante o Fullstack Lab do Dev Pleno
+            <br />
+            Implementado por:{' '}
+            <a className='text-gray-700' href='https://www.linkedin.com/in/igor-pedro-martins-igooruh/'>Ígor Pedro Martins</a>
+        </footer>
+    )
+}
+
+export default Footer
