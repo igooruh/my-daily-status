d0c082c494d81f2e6592a50bae0da31c06d5eada
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-04-28 02:36:45 -0300

    Refactory validate function of user session

--- pages/app.js
+++ pages/app.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import router from 'next/router'
 
 import auth0 from '../lib/auth0'
 
-const App = () => {
+const App = props => {
+
+    useEffect(() => {
+        if(!props.isAuth) {
+            router.push('/')
+        }
+    })
+
+    if(!props.isAuth) {
+        return null
+    }
 
     return (
-        <h1>APP</h1>
+        <section>
+            <h1>APP</h1>
+            <pre>{ JSON.stringify(props, null, 2) }</pre>
+        </section>
     )
 }
 
@@ -13,12 +27,20 @@ export default App
 
 export async function getServerSideProps({ req, res }) {
 
-    const session = auth0.getSession(req)
+    let user = {}
+    let isAuth = false
+
+    const session = await auth0.getSession(req)
+
+    if(session) {
+        isAuth = true
+        user = session.user
+    }
+
     return {
         props: {
-            user: {
-
-            }
+            isAuth,
+            user
         }
     }
 }
