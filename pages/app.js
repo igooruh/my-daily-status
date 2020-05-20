c490deb80ba20518bad5016f6bfd01ff10f800fb
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-05-20 00:56:00 -0300

    Refactory importing function of other file

--- pages/app.js
+++ pages/app.js
@@ -3,7 +3,7 @@ import router from 'next/router'
 
 import auth0 from '../lib/auth0'
 import { db } from '../lib/db' 
-import FunctionHelp from '../lib/functions-help'
+import { checkExistsUser, findChecksNearbyCheckin } from '../lib/markers'
 
 const App = props => {
 
@@ -44,46 +44,13 @@ export async function getServerSideProps({ req, res }) {
     const session = await auth0.getSession(req)
 
     if(session) {
+        const todaysCheckin = await checkExistsUser(session.user.sub)
 
-        const currentDate = FunctionHelp.currentDateToday()
-        
-        const todaysCheckin = await db
-            .collection('markers')
-            .doc(currentDate)
-            .collection('checks')
-            .doc(session.user.sub)
-            .get()
-
-        const todaysDate = todaysCheckin.data()
+        const todaysData = todaysCheckin.data()
         let forceCreate = true
-        if(todaysDate) {
+        if(todaysData) {
             forceCreate = false
-            const checkins = await db.collection('markers')
-                .doc(currentDate)
-                .collection('checks')
-                .near({
-                    center: todaysDate.coordinates,
-                    radius: 1000 
-                })
-                .get()
-
-            const checkinsList = []
-            checkins.docs.forEach(doc => {
-                checkinsList.push({
-                    id: doc.id,
-                    status: doc.data().status,
-                    coords: {
-                        lat: doc.data().coordinates.latitude,
-                        long: doc.data().coordinates.longittude
-                    },
-                    distance: FunctionHelp.distanceCoordinates(
-                        todaysDate.coordinates.latitude,
-                        todaysDate.coordinates.longitude,
-                        doc.data().coordinates.latitude,
-                        doc.data().coordinates.longittude
-                    ).toFixed(2)
-                })
-            })
+            const checkinsList = await findChecksNearbyCheckin(todaysData)
 
             return {
                 props: {
