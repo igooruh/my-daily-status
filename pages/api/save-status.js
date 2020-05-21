9f68265d12d287dcc28ef0ff120011d00b8b3c32
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-05-21 00:56:41 -0300

    Refactory code

--- pages/api/save-status.js
+++ pages/api/save-status.js
@@ -2,32 +2,18 @@ import admin from 'firebase-admin'
 
 import { db } from '../../lib/db'
 import auth0 from '../../lib/auth0'
-import FunctionHelper from '../../lib/functions-help'
+import { setStatus } from '../../lib/markers'
 
 const saveStatus = async(request, response) => {
 
     const session = await auth0.getSession(request)
-    
+
     if(session) {
         const datas = request.body
-        const currentDate = FunctionHelper.currentDateToday()
-
-        await db
-            .collection('markers')
-            .doc(currentDate)
-            .collection('checks')
-            .doc(session.user.sub)
-            .set({
-                status: datas.status,
-                user: session.user.sub,
-                coordinates: new admin.firestore.GeoPoint(
-                    datas.coords.lat,
-                    datas.coords.long
-                )
-            })
-
-        response.send({ ok: true })
+        await setStatus(session.user.sub, datas)
     }
+
+    response.send({ ok: true })
 }
 
 export default saveStatus
